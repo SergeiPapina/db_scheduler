bbfe1e43f5278a8db198c38fbd5f743be06ac898
SPapina <papina.sergei@gmail.com>
2024-09-03 19:28:14 +0300

    run commands

--- runner.py
+++ runner.py
@@ -8,14 +8,20 @@ from subprocess import run
 
 counter = 0
 
+async def run_selenium():
+    cmd_selenium = "java '-Dwebdriver.chrome.driver=d:\work\Selenium\chromedriver.exe' -jar 'D:\work\Selenium\selenium-server-standalone-3.5.3.jar'"
+    try:
+        result = run(cmd_selenium, shell=True, check=True, text=True)
+        print(result)
+    except subprocess.CalledProcessError as e:
+        pass
 
 async def run_parser():
     global counter
-    cmd = f'pip list | grep {counter}'
-    print(f"start page {counter}")
+    cmd_microparser = "mvn test -Dsuite=testng"
     counter += 1
     try:
-        result = run(cmd, shell=True, check=True, text=True)
+        result = run(cmd_microparser, shell=True, check=True, text=True)
         print(result)
     except subprocess.CalledProcessError as e:
         pass
@@ -24,13 +30,11 @@ async def run_parser():
 async def main():
     schedule = Scheduler()
 
-    schedule.once(dt.timedelta(seconds=5), run_parser)
-    schedule.cyclic(dt.timedelta(seconds=10), run_parser)
+    schedule.once(dt.timedelta(seconds=1), run_selenium)
+    schedule.cyclic(dt.timedelta(seconds=60*60*24), run_parser)
 
     while counter < 27:
         await asyncio.sleep(1)
-
-    print("bye")
 
 
 asyncio.run(main())
